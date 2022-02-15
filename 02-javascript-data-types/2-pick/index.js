d40ec4125d6329dd67fbbae6b77d548d9c45407a
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-02-15 14:28:25 +0300

    create pick and omit functions

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const keysValues = Object.entries(obj);
+  const newObj = {};
+  for (const keyValue of keysValues) {
+    for (const field of fields) {
+      if (keyValue[0] === field) {
+        newObj[field] = keyValue[1];
+      }
+    }
+  }
+  return newObj;
 };
