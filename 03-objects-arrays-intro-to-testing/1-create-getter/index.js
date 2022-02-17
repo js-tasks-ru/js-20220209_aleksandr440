387a49107e41c37a14b7b3dbe3cbf70df6fbb504
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-02-17 15:50:15 +0300

    create function createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,15 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
-
-}
+export const createGetter = path => {
+  return obj => {
+    const [firstLevel, ...paths] = path.split('.');
+    let value = obj[firstLevel];
+    if (value) {
+      paths.forEach((item) => {
+        value = value[item];
+      });
+    }
+    return value;
+  };
+};
