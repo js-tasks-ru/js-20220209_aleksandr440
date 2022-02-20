1cd2e832b57d0928fcc44942988a25b6cfaf91ec
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-02-20 22:45:01 +0300

    added function trimSimbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,20 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export const trimSymbols = (string, size) => {
+  if (size === 0) {
+    return '';
+  } 
+  if (string === undefined) {
+    return string;
+  } 
+  const firstSlice = string.slice(0, size);
+  const rest = [...string.slice(size)];
 
-}
+  return rest.reduce((acc, symbol) => {
+    if (!acc.endsWith(symbol.repeat(size))) {
+      acc += symbol;
+    }
+    return acc;
+  }, firstSlice);
+};
